53bd8477369636ff9ab49cb63256ca6340334b0f
MohamadSallakhou <s0592381@htw-berlin.de>
2024-10-17 19:16:59 +0200

    1

---
+++ src/test/java/de/htwberlin/webtech/Ubung1/Ubung1ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.Ubung1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ubung1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
