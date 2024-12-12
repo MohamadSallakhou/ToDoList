70d51e74763099dd49d6e2b55cd5135674725442
MohamadSallakhou <s0592381@htw-berlin.de>
2024-12-12 19:19:16 +0100

    Gradle

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,12 +3,12 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-public class DemoApplicationTests {
+//@SpringBootTest
+//public class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-		// Überprüft, ob der Spring Application Context erfolgreich geladen wird
-	}
+//	@Test
+//	void contextLoads() {
+//		// Überprüft, ob der Spring Application Context erfolgreich geladen wird
+//	}
 
-}
+//}
