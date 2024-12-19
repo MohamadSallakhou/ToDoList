f9eff2e3e8ea36ea4ea75e41e5ed3359a6233cd1
Nouran <nouranha122@gmail.com>
2024-12-19 18:59:00 +0100

    add database and change the Taskservice and add TaskRepository

--- src/main/java/de/htwberlin/webtech/DemoApplication.java
+++ src/main/java/de/htwberlin/webtech/DemoApplication.java
@@ -4,6 +4,8 @@ import de.htwberlin.webtech.model.Task;
 import de.htwberlin.webtech.service.TaskService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,6 +16,8 @@ import java.util.List;
 
 // Entry Point
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "de.htwberlin.webtech.repository")
+@EntityScan(basePackages = "de.htwberlin.webtech.model")
 class TodoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
--- src/main/java/de/htwberlin/webtech/model/Task.java
+++ src/main/java/de/htwberlin/webtech/model/Task.java
@@ -1,13 +1,14 @@
 package de.htwberlin.webtech.model;
 
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
 // Task-Klasse
-
+@Entity
 @Getter
 @Setter
 @ToString
